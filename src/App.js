d74285f1dfb3ae5a73df74739d0c75a1ef06c7c4
Joao_Godinho <git.qodeless@outlook.com>
2022-02-27 15:48:10 -0300

    styled components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Title, TitleSmall } from "./styles";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Title fontSize={20}>
+          <h1>Hello World</h1>
+          <span>texto menor</span>
+        </Title>
+
+        <TitleSmall>Menor</TitleSmall>
+      </div>
+    );
+  }
 }
 
 export default App;
